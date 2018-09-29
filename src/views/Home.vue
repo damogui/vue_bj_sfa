e471502cc2333431630e2281bc893118a27c35af
malun <malun666@126.com>
2018-09-29 18:13:10 +0800

    added the hotcss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <H1>我是bj sfa项目</H1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login">
+    <h1>登录页面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../lib/hotcss/px2rem.scss";
+h1 {
+  width: px2rem(375);
+  background-color: #c09;
+  height: px2rem(40);
+}
+</style>
