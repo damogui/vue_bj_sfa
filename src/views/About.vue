05b4a95c60989c3caef8c7deb4aef445466349a2
malun <malun666@126.com>
2018-09-29 18:56:45 +0800

    added the login bgc

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,9 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<style lang="scss">
+h1 {
+  width: px2rem(100);
+  background-color: #c33;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login">
-    <h1>登录页面</h1>
+    <h1>欢迎登录宝洁SFA系统</h1>
   </div>
 </template>
 
@@ -12,12 +12,27 @@ export default {
   }
 };
 </script>
+<style lang="scss">
+html,
+body,
+#app {
+  height: 100%;
+}
+</style>
 
 <style lang="scss" scoped>
-@import "../lib/hotcss/px2rem.scss";
+/* @import "../lib/hotcss/px2rem.scss"; */
 h1 {
-  width: px2rem(375);
-  background-color: #c09;
-  height: px2rem(40);
+  text-align: center;
+  font-size: px2rem(36);
+  height: px2rem(148);
+  padding: px2rem(148-36-44) 0 px2rem(44) 0;
+  line-height: px2rem(36);
+  color: #fff;
+}
+
+.login {
+  background-color: #2ade69;
+  height: 100%;
 }
 </style>
